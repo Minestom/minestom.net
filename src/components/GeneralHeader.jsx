71f0b740fb94fa345f24e0221fd1a96c7e872004
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-08-26 15:17:11 +0200

    Resize favicon to 240x240

--- src/components/GeneralHeader.jsx
+++ src/components/GeneralHeader.jsx
@@ -6,7 +6,7 @@ export default class GeneralHeader extends Component {
         return (
             <Helmet htmlAttributes={{lang: "en"}}>
                 <meta name="theme-color" content="#2A3030" />
-                <link href="static/img/icon_transparent.png" rel="icon" />
+                <link href="static/img/icon_transparent_240.png" rel="icon" />
                 <script async src="https://kit.fontawesome.com/7f109a5094.js" crossorigin="anonymous" />
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-BREZVMFMW6" />
                 <script async src="/static/js/gtag.js" />
