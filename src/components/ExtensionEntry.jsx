a3c3f54ec3ff9a6da9c2c9cad799042d3d1d81fc
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-08-28 21:37:55 +0200

    Change official highlight design

--- src/components/ExtensionEntry.jsx
+++ src/components/ExtensionEntry.jsx
@@ -5,12 +5,9 @@ export default class ExtensionEntry extends Component {
     render() {
         return (
             <li className="extension-entry">
-                <div>
+                <div className={this.props.owner.login === "Minestom" && "official"}>
                     <a href={"/extension?id="+this.props.owner.login+"/"+this.props.name} rel="nofollow">
-                        <h3>
-                            {this.props.owner.login === "Minestom" && (<img src="/static/img/icon_transparent.png" height="16px" style={{display: "inline-block", marginRight: "4px"}} />)}
-                            {this.props.name}
-                        </h3>
+                        <h3>{this.props.name}</h3>
                         <h4>by <a rel="nofollow" href={"https://github.com/"+this.props.owner.login}>{this.props.owner.login}</a></h4>
                         <p>{this.props.description}</p>
                     </a>
