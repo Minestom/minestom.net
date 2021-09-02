8f6beec5c51a065014fe2f25b3552f1f37cb3b91
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-09-02 20:40:14 +0000

    Replace gh icon and increase it's size to 150%

--- src/components/ExtensionEntry.jsx
+++ src/components/ExtensionEntry.jsx
@@ -9,7 +9,7 @@ export default class ExtensionEntry extends Component {
                     <a href={"/extension?id="+this.props.owner+"/"+this.props.name} rel="nofollow">
                         <h3>{this.props.name}</h3>
                         <h4>by <a rel="nofollow" href={"https://github.com/"+this.props.owner}>{this.props.owner}</a></h4>
-                        <a className={"direct-link"} href={"https://github.com/"+this.props.owner+"/"+this.props.name}><i className="fab fa-github-square"/></a>
+                        <a className={"direct-link"} href={"https://github.com/"+this.props.owner+"/"+this.props.name}><i className="fab fa-github"/></a>
                         <p>{this.props.description}</p>
                     </a>
                 </div>
