519a90b754976bc7c4f54d1e6dda9a0bf7e847e7
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-09-04 17:29:48 +0200

    Make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,29 @@ import ExternalLinks from '../resources/external-links.json'
 import InternalLinks from '../resources/internal-links.json'
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this)
+        this.state = {
+            toggled: false
+        }
+    }
 
     render() {
         return (
             <div className="navbar">
                 <span>
-                    {InternalLinks.map(link => (<NavbarLink {...link} />))}
+                    <i className="fas fa-bars" onClick={this.toggle} />
+                    <span className={`toggle-container ${this.state.toggled ? "on" : "off"}`}>
+                        {InternalLinks.map(link => (<NavbarLink {...link} />))}
+                    </span>
                 </span>
                 {ExternalLinks.map(link => (<NavbarLink {...link} />))}
             </div>
         )
+    }
+
+    toggle() {
+        this.setState({toggled: !this.state.toggled})
     }
 }
