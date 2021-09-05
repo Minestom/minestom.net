75b11f71ff14c5d689d79afe2a5b1d48276c3de2
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-09-05 11:51:17 +0200

    Implement search

---
+++ src/components/Search.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Fuse from "fuse.js";
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.handleInput = this.handleInput.bind(this)
+
+        this.fuse = null
+        if (this.props.data !== undefined) {
+            this.fuse = new Fuse(this.props.data, {
+                keys: this.props.keys,
+                threshold: .25
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <i className="fas fa-search"/>
+                <input type={"text"} placeholder={"Search"} onInput={this.handleInput}/>
+            </div>
+        );
+    }
+
+    handleInput(event) {
+        const value = event.target.value;
+
+        if(value.length === 0 || this.fuse === null) {
+            this.props.onResult({
+                useResults: false
+            })
+            return
+        }
+
+        this.props.onResult({
+            useResults: true,
+            results: this.fuse.search(value).map(x => x.item)
+        })
+    }
+}
+
+export default Search;
