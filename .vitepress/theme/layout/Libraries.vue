e9218fcbd664bb43b652b7809652f21d9d75c338
mworzala <mattheworzala@gmail.com>
2024-06-18 07:17:25 -0600

    Revert "fix: take results from libraries api"
    This reverts commit da5f6754e693582fe0251bc0a328fdb1dc4080ca.

--- .vitepress/theme/layout/Libraries.vue
+++ .vitepress/theme/layout/Libraries.vue
@@ -69,13 +69,13 @@ export default {
             this.loading = true;
             try {
                 const response = await axios.get("/api/libraries");
-                if (!Array.isArray(response.data.results)) {
+                if (!Array.isArray(response.data)) {
                     console.error(
                         "Malformed response from server when requesting libraries",
                     );
                     return;
                 }
-                this.libraries = response.data.results;
+                this.libraries = response.data;
             } catch (error) {
                 console.error("Error fetching libraries:", error);
                 this.libraries = [];
